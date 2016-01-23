089084c1f6922cacc1414cebe18298efc51af50a
Kaze <kaze-d@outlook.com>
2016-01-23 15:42:36 +0100

    injection order fixes

--- public/application.js
+++ public/application.js
@@ -1,5 +1,27 @@
 'use strict';
 
+// Init the application configuration module for AngularJS application
+var ApplicationConfiguration = (function() {
+    // Init module configuration options
+    var applicationModuleName = 'mediacollection';
+    var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+
+    // Add a new vertical module
+    var registerModule = function(moduleName, dependencies) {
+        // Create angular module
+        angular.module(moduleName, dependencies || []);
+
+        // Add the module to the AngularJS configuration file
+        angular.module(applicationModuleName).requires.push(moduleName);
+    };
+
+    return {
+        applicationModuleName: applicationModuleName,
+        applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+        registerModule: registerModule
+    };
+})();
+
 //Start by defining the main module and adding the module dependencies
 angular.module(ApplicationConfiguration.applicationModuleName, ApplicationConfiguration.applicationModuleVendorDependencies);
 
