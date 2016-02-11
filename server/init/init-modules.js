f393ac974a3af3609828e8afa8f2a3c553ac690b
Kaze <kaze-d@outlook.com>
2016-02-11 21:54:25 +0100

    migration of isbn search route

--- server/init/init-modules.js
+++ server/init/init-modules.js
@@ -3,7 +3,9 @@
 module.exports = (server) => {
     const users = require('../modules/users');
     const books = require('../modules/books');
+    const googleBookApi = require('../modules/google-book-api');
 
     server.register(users.module, users.options);
     server.register(books.module, books.options);
+    server.register(googleBookApi.module, googleBookApi.options);
 };
