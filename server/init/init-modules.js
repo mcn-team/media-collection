9e770c84666fb1476eee88267729f9fecfae3695
Kaze <kaze-d@outlook.com>
2016-02-14 21:03:53 +0100

    authentication module added

--- server/init/init-modules.js
+++ server/init/init-modules.js
@@ -3,11 +3,13 @@
 module.exports = (server) => {
     const users = require('../modules/users');
     const books = require('../modules/books');
+    const auth = require('../modules/auth');
     const googleBookApi = require('../modules/google-book-api');
     const wikipediaApi = require('../modules/wikipedia-api');
 
     server.register(users.module, users.options);
     server.register(books.module, books.options);
+    server.register(auth.module);
     server.register(googleBookApi.module, googleBookApi.options);
     server.register(wikipediaApi.module, wikipediaApi.options);
 };
