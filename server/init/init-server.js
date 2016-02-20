efc735e46fe476579a64ab5ded18e88563d0cecf
Matthias Brunet <kaze-d@outlook.com>
2016-02-21 00:20:52 +0100

    options to log files request added

--- server/init/init-server.js
+++ server/init/init-server.js
@@ -1,12 +1,27 @@
 'use strict';
 
-const logger = require('../utils/logger');
 const _ = require('lodash');
 
+const logger = require('../utils/logger');
+const config = require('../config');
+
 module.exports = (server) => {
-    server.ext('onPreResponse', (request, reply) => {
+    const writeLog = (request) => {
         const statusCode = request.response.statusCode || request.response.output.statusCode || '???';
         logger.logRoute(statusCode, _.upperCase(request.method), request.path);
+    };
+
+    server.ext('onPreResponse', (request, reply) => {
+        const isApi = !(_.indexOf(request.path, '.') > 0) && request.path !== '/';
+
+        if (isApi && config.logRoute) {
+            writeLog(request);
+        }
+
+        if (!isApi && config.logFiles) {
+            writeLog(request);
+        }
+
         reply.continue();
     });
 };
