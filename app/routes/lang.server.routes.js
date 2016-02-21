d0ae427d645bd4d27357250197d48e51355990a4
Matthias Brunet <kaze-d@outlook.com>
2016-02-21 22:49:15 +0100

    express server deleted

--- app/routes/lang.server.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-    var users = require('../../app/controllers/users.server.controller');
-    var lang = require('../../app/controllers/lang.server.controller');
-
-    app.route('/lang')
-        .get(lang.getLang);
-};
--- app/routes/my-api-film-api.server.routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-    var users = require('../../app/controllers/users.server.controller');
-    var myApiFilm = require('../../app/controllers/my-api-film-api.server.controller');
-
-    app.route('/api/myApiFilm/latestTrailer')
-        .get(myApiFilm.getLastTrailer);
-};
