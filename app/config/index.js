728559906caed53bb1912a5e1e19b5c8830193ac
Matthias Brunet <kaze-d@outlook.com>
2016-03-07 23:19:54 +0100

    config loader and validation updated

--- app/config/index.js
+++ app/config/index.js
@@ -1,21 +1,12 @@
 'use strict';
 
-const localConfig = require('./config.json');
-const _ = require('lodash');
+const config = require('./config.json');
 const validation = require('./validation');
 
-let defaultConfig = {
-    connections: [
-        { port: 4000 }
-    ],
-    dbConnectionString: 'mongodb://localhost:27017/mc',
-    colors: true
-};
-
-validation.validate(_.merge(defaultConfig, localConfig), (err) => {
+validation.validate(config, (err) => {
     if (err) {
         throw err;
     }
 });
 
-module.exports = defaultConfig;
+exports = module.exports = config;
--- app/config/validation.js
+++ app/config/validation.js
@@ -2,16 +2,39 @@
 
 const Joi = require('joi');
 
+const fileSchema = {
+    relativeTo: Joi.string().required()
+};
+
+const corsSchema = {
+    origin: Joi.array().min(1).items(Joi.string()).required(),
+    additionalHeaders: Joi.array().min(1).items(Joi.string()).optional()
+};
+
+const routeSchema = {
+    files: Joi.object(fileSchema).required(),
+    cors: Joi.object(corsSchema).optional()
+};
+
+const connectionSchema = {
+    host: Joi.string().required(),
+    port: Joi.number().min(1025).max(65535).required(),
+    labels: Joi.array().min(1).required(),
+    routes: Joi.object(routeSchema).required()
+};
+
 const configSchema = {
-    connections: Joi.array().min(1).items(Joi.object({ port: Joi.number().min(1025).max(65535).required() }).required()),
+    connections: Joi.array().min(1).items(Joi.object(connectionSchema).required()).required(),
     dbConnectionString: Joi.string().regex(/^mongodb:\/\/[a-z]+[:][0-9]+\/.+/).required(),
-    color: Joi.boolean(),
+    colors: Joi.boolean().required(),
+    logRoute: Joi.boolean().required(),
+    logFiles: Joi.boolean().required(),
     secretJWT: Joi.string().required(),
-    clientPath: Joi.string()
+    clientPath: Joi.string().required()
 };
 
 exports.validate = (config, callback) => {
-    Joi.validate(config, configSchema, { allowUnknown: true }, (err, value) => {
+    Joi.validate(config, configSchema, { allowUnknown: false }, (err, value) => {
         return callback(err, value);
     });
 };
