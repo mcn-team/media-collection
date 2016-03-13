f264d10854ce54882916c064cae864f1103ae579
Matthias Brunet <kaze-d@outlook.com>
2016-03-13 01:27:29 +0100

    book route API documentation added

--- app/modules/books/routes.js
+++ app/modules/books/routes.js
@@ -8,7 +8,9 @@ module.exports = (server) => {
         method: 'GET',
         path: '/',
         config: {
-            auth: 'RequiresLogin'
+            auth: 'RequiresLogin',
+            notes: 'Returns an array of Books model',
+            description: 'Sends back the full book list registered in the database.'
         },
         handler: controller.getAllBooks
     });
@@ -18,7 +20,10 @@ module.exports = (server) => {
         path: '/',
         config: {
             auth: 'RequiresLogin',
-            validate: { payload: validator.bookCreatePayload }
+            validate: { payload: validator.bookCreatePayload },
+            notes: [ 'Returns a Book model' ],
+            description: 'Takes a book object as payload, adds it to the database ' +
+            'and sends back the newly added instance of Book model.'
         },
         handler: controller.createBook
     });
@@ -28,7 +33,13 @@ module.exports = (server) => {
         path: '/{bookId}',
         config: {
             auth: 'RequiresLogin',
-            validate: { params: validator.bookParams }
+            validate: { params: validator.bookParams },
+            notes: [
+                'Takes a Book ID as parameter',
+                'Returns a single Book model'
+            ],
+            description: 'Sends back the Book model corresponding to the ' +
+            'Mongo ObjectID passed as parameter.'
         },
         handler: controller.getBook
     });
@@ -41,7 +52,15 @@ module.exports = (server) => {
             validate: {
                 params: validator.bookParams,
                 payload: validator.bookEditPayload
-            }
+            },
+            notes: [
+                'Takes a Book ID as parameter',
+                'Receives a partial/complete Book object as payload',
+                'Returns a single Book model'
+            ],
+            description: 'Edit the book in the database corresponding to the ID parameter ' +
+            'and sends back the corresponding Book model. Book model can be partially edited ' +
+            'by passing an object containing only the fields to update.'
         },
         handler: controller.updateBook
     });
@@ -50,7 +69,9 @@ module.exports = (server) => {
         method: 'GET',
         path: '/latest',
         config: {
-            auth: 'RequiresLogin'
+            auth: 'RequiresLogin',
+            notes: 'Returns a single Book model',
+            description: 'Sends back the lastest Book model added to the database.'
         },
         handler: controller.getLatestBook
     });
@@ -59,16 +80,23 @@ module.exports = (server) => {
         method: 'GET',
         path: '/collections',
         config: {
-            auth: 'RequiresLogin'
+            auth: 'RequiresLogin',
+            notes: 'Returns an array of Object',
+            description: 'Sends back an aggregate array or object with an "_id" field ' +
+            'containing the collection\'s name and a "data" field containing an array of ' +
+            'all Book models sharing the specified collection\'s name.'
         },
         handler: controller.getCollectionsList
     });
 
     server.route({
         method: 'GET',
-        path: '/names',
+        path: '/collections/names',
         config: {
-            auth: 'RequiresLogin'
+            auth: 'RequiresLogin',
+            notes: 'Returns an array of Strings',
+            description: 'Sends back an array containing every collection\'s name registered ' +
+            'in the database.'
         },
         handler: controller.getCollectionNamesList
     });
@@ -78,7 +106,15 @@ module.exports = (server) => {
         path: '/collections/{collection}/volumes/{volume}',
         config: {
             auth: 'RequiresLogin',
-            validate: { params: validator.collectionsParams }
+            validate: { params: validator.collectionsParams },
+            notes: [
+                'Takes an collection\'s name as parameter',
+                'Takes a volume number as parameter',
+                'Returns an array of Book models'
+            ],
+            description: 'Sends back an array containing the list of all books ' +
+            'with the corresponding collection and a lower volume number ' +
+            'registered in the database.'
         },
         handler: controller.getCollection
     });
@@ -88,7 +124,13 @@ module.exports = (server) => {
         path: '/{bookId}',
         config: {
             auth: 'RequiresLogin',
-            validate: { params: validator.bookParams }
+            validate: { params: validator.bookParams },
+            notes: [
+                'Takes a Book ID as parameter',
+                'Returns 204 No Content'
+            ],
+            description: 'Removes the book whit the specified Mongo ObjectID ' +
+            'passed as parameter from the database.'
         },
         handler: controller.deleteBook
     });
@@ -97,7 +139,12 @@ module.exports = (server) => {
         method: 'POST',
         path: '/many',
         config: {
-            auth: 'RequiresLogin'
+            auth: 'RequiresLogin',
+            notes: [
+                'Receives an array of Book object as payload',
+                'Returns 204 No Content'
+            ],
+            description: 'Adds multiple Book model to database.'
         },
         handler: controller.createMultiple
     });
