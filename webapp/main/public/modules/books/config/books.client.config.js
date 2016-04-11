bf4ddecccb202607b2a403a5ea27d8d70dfed4b4
Matthias Brunet <matthias.brunet@viseo.com>
2016-04-11 11:54:04 +0200

    header translation completed
    language service references removed

--- webapp/main/public/modules/books/config/books.client.config.js
+++ webapp/main/public/modules/books/config/books.client.config.js
@@ -2,17 +2,11 @@
 
 // Configuring the Articles module
 angular.module('books').run([
-    'Menus', 'LanguageServices',
-    function(Menus, LanguageServices) {
-        LanguageServices.fetchLanguages(function (err) {
-            if (!err) {
-                var fields = LanguageServices.lang['en'].home;
-                // Set top bar menu items
-                Menus.addMenuItem('topbar', 'BOOK_HEADER', 'books', 'dropdown', '/books(/create)?');
-                Menus.addSubMenuItem('topbar', 'books', 'BOOK_LIST', 'books');
-                Menus.addSubMenuItem('topbar', 'books', 'BOOK_COLLECTION', 'books/collections');
-                Menus.addSubMenuItem('topbar', 'books', 'ADD_BOOK', 'books/create');
-            }
-        });
+    'Menus',
+    function(Menus) {
+        Menus.addMenuItem('topbar', 'BOOK_HEADER', 'books', 'dropdown', '/books(/create)?');
+        Menus.addSubMenuItem('topbar', 'books', 'BOOK_LIST', 'books');
+        Menus.addSubMenuItem('topbar', 'books', 'BOOK_COLLECTION', 'books/collections');
+        Menus.addSubMenuItem('topbar', 'books', 'ADD_BOOK', 'books/create');
     }
 ]);
