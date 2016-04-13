44f79db6dff9e64cc5173d1f43405fcd30e07b9a
Matthias Brunet <kaze-d@outlook.com>
2016-04-13 22:31:50 +0200

    user update api call added

--- webapp/main/public/modules/options/components/users/user-edit.modal.js
+++ webapp/main/public/modules/options/components/users/user-edit.modal.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('options').controller('UserModalCtrl', [
-    '$scope', '$uibModalInstance', 'lodash', 'user',
-    function ($scope, $uibModalInstance, _, user) {
+    '$scope', '$uibModalInstance', 'lodash', 'UserServices', 'user',
+    function ($scope, $uibModalInstance, _, UserServices, user) {
         var originalUser = _.assign({}, user);
         $scope.user = user;
 
@@ -15,7 +15,15 @@ angular.module('options').controller('UserModalCtrl', [
                 }
             });
 
-            $uibModalInstance.close(edited);
+            var successCallback = function () {
+                $uibModalInstance.close();
+            };
+
+            var failureCallback = function (errorResponse) {
+                console.error(errorResponse);
+            };
+
+            UserServices.updateUser($scope.user._id, edited).then(successCallback, failureCallback);
         };
 
         $scope.dismissModal = function () {
