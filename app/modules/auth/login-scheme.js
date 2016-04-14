3e9df33be205ad41c546dcb86b4c9cdb66c0e5df
Matthias Brunet <matthias.brunet@viseo.com>
2016-04-14 15:18:02 +0200

    validation login scheme updated
    requires admin strategy added
    
    scheme is a big copy paste of previous scheme
    find a way to refactorise it

---
+++ app/modules/auth/login-scheme.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+const User = require('../users/model').model;
+const config = require('../../config');
+
+exports = module.exports = (server, options) => {
+    return {
+        authenticate: (request, reply) => {
+            const token = request.headers['auth-web-token'];
+
+            if (!token) {
+                return reply({ message: 'Please log in' }).code(401);
+            }
+
+            jwt.verify(token, config.secretJWT, (err, decoded) => {
+                if (err) {
+                    return reply({ message: 'Token is not valid', error: err }).code(401);
+                }
+
+                User.findOne().where({ _id: decoded.user }).exec((err, user) => {
+                    if (err) {
+                        return reply({ error: err }).code(503);
+                    } else if (!user) {
+                        return reply({ message: 'user does not exists', error: err }).code(401);
+                    } else if (options && options.admin && user && !user._doc.admin) {
+                        return reply({ message: 'You are not administrator' }).code(403);
+                    }
+
+                    return reply.continue({ credentials: { id: decoded.user } })
+                });
+            });
+        }
+    };
+};
