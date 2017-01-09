5c6354534a3412c436afdb95ba6718daf55022af
Matthias Brunet <matthias.brunet@viseo.com>
2017-01-09 21:55:12 +0100

    menues labels and order updated

--- webapp/main/public/modules/movies/config/movies.client.config.js
+++ webapp/main/public/modules/movies/config/movies.client.config.js
@@ -5,8 +5,8 @@ angular.module('movies').run([
     'Menus',
     function(Menus) {
         Menus.addMenuItem('topbar', 'MOVIE_HEADER', 'movies', 'dropdown', '/movies(/create)?', null, null, 1);
+        Menus.addSubMenuItem('topbar', 'movies', 'ADD_MOVIE', 'movies/create');
         Menus.addSubMenuItem('topbar', 'movies', 'MOVIE_LIST', 'movies');
         Menus.addSubMenuItem('topbar', 'movies', 'MOVIE_COLLECTION', 'movies/collections');
-        Menus.addSubMenuItem('topbar', 'movies', 'ADD_MOVIE', 'movies/create');
     }
 ]);
